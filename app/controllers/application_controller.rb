9ed487fcddb631ff224f7788809ee9534edad127
kelvin <kelvin86@gmail.com>
2015-08-15 04:44:04 +0000

    Added sessions, log in and out functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  #methods are in default available only to controller
+  #To allow methods in views, use helper_method:
+  
+  helper_method :current_user, :logged_in?
+  
+  
+  def current_user
+    @current_user ||= Chef.find(session[:chef_id]) if session[:chef_id]
+  end
+  
+  def logged_in?
+    #remember, !!current_user points to the method, not the variable @current user!
+    !!current_user
+    
+    #Wrong return
+    #!!@current_user
+    
+    
+    
+  end
+  
+  
 end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -11,5 +11,7 @@ class Chef < ActiveRecord::Base
     has_many :recipes
     has_many :likes
     
+    has_secure_password
+    
     
 end
